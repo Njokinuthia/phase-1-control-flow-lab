c0e8a2fceb3cb682e2a20c6e4dc15cb117d8ff24
Njoki <kinuthiash@gmail.com>
2022-04-08 21:42:54 +0300

    add distance and city functions

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if(distance <= 400){
+    return "This one is on me!"
+  }
+  else if(distance > 400 && distance <= 2000){
+    return "That will be twenty bucks."
+  }
+  else if (distance > 2000 && distance <= 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (distance > 2500){
+    return "No can do."
+  }
+  else{
+    return "enter a distance"
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(1500))
+
+function ternaryCheckCity(city){
+
+ if (city === "NYC"){
+   return "Ok, sounds good."
+ }
+ else{
+   return "No go."
+ }
 }
+
+console.log(ternaryCheckCity("Naiton"));
 
 function switchOnCharmFromTip(){
   // Write your code here!
